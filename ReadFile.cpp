2400f351f11dd02f289c0cf22fcb4acd4fe0d204
Stavros Kalaouzis <stavros.kalaouzis@yahoo.gr>
2016-09-06 15:16:34 +0300

    Update ReadFile.cpp

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -1,17 +1,58 @@
 #include <fstream>
-#include <string>
 #include <iostream>
- 
-int main( int argc , char** argv ) {
-   int linecount = 0 ;
-   std::string line ;
-   std::ifstream infile( argv[ 1 ] ) ;
-   if ( infile ) {
-      while ( getline( infile , line ) ) {
-	 std::cout << linecount << ": " << line << '\n' ;//supposing '\n' to be line end
-	 linecount++ ;
-      }
-   }
-   infile.close( ) ;
-   return 0 ;
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	//-------------------------------------------------------
+	//Create (writting) a txt file...
+	//ofstream myfile("example.txt");
+	//if (myfile.is_open())
+	//{
+	//	myfile << "This is a line sdfsd.\n";
+	//	myfile << "This is another line.\n";
+	//	myfile.close();
+	//}
+	//else cout << "Unable to open file";
+	//------------------------------------------------------
+	
+	
+	string str2 = "world";
+		
+	string line;
+	ifstream myfile("example.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			cout << line << '\n';
+			str2 = line;
+
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	//------------------------------------------------------
+
+
+	string str1 = "Hello";
+	
+	string str3;
+	int  len;
+
+	// copy str1 into str3
+	str3 = str1;
+	cout << "str3 : " << str3 << endl;
+
+	// concatenates str1 and str2
+	str3 = str1 + str2;
+	cout << "str1 + str2 : " << str3 << endl;
+
+	// total lenghth of str3 after concatenation
+	len = str3.size();
+	cout << "str3.size() :  " << len << endl;
+
+	return 0;
 }
